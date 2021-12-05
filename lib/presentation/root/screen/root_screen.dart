29e98c0c6ea7035504f72ffc317fc583b5fe6675
Catyousha <jakaasha@gmail.com>
2021-12-05 14:30:23 +0700

    add filled icons on active page

--- lib/presentation/root/screen/root_screen.dart
+++ lib/presentation/root/screen/root_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconly/iconly.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import '../../../config/themes/themes.dart';
@@ -17,25 +18,25 @@ class RootScreen extends StatelessWidget {
   List<PersistentBottomNavBarItem> _navbarItems() {
     return [
       PersistentBottomNavBarItem(
-        icon: const Icon(IconlyLight.home),
+        icon: const Icon(IconlyBold.home),
+        inactiveIcon: const Icon(IconlyLight.home),
         title: 'Home',
         activeColorPrimary: AppColor.primary,
         inactiveColorPrimary: Colors.grey,
-        //textStyle: AppText.textSmall.copyWith(fontWeight: AppText.semiBold),
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(IconlyLight.bag),
+        icon: const Icon(IconlyBold.bag),
+        inactiveIcon: const Icon(IconlyLight.bag),
         title: 'Market',
         activeColorPrimary: AppColor.primary,
         inactiveColorPrimary: Colors.grey,
-        //textStyle: AppText.textSmall.copyWith(fontWeight: AppText.semiBold),
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(IconlyLight.profile),
+        icon: const Icon(IconlyBold.profile),
+        inactiveIcon: const Icon(IconlyLight.profile),
         title: 'Profile',
         activeColorPrimary: AppColor.primary,
         inactiveColorPrimary: Colors.grey,
-        //textStyle: AppText.textSmall.copyWith(fontWeight: AppText.semiBold),
       ),
     ];
   }
